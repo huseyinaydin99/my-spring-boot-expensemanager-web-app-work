d321061e639a0edee6aca8195fb11cd379f076cd
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-10-07 15:33:47 +0300

    Harcama yöneticisi Web uygulamasıdır. Sisteme kayıt olan kullanıcı giriş yaptıktan sonra yaptığı harcalamaları ve hesaplamalarını yönetir. Spring Boot + Spring Security + Spring Data JPA + Validation + Model Mapper + MySQL + Postgresql + Thymeleaf + N-Tier Architechture + ORM + IoC + DI + Strategy Desing Pattern + Repository Desing Pattern teknolojileri ve teknikleri kullanılmıştır.

--- src/main/java/tr/com/huseyinaydin/expensemanager/dto/ExpenseFilterDTO.java
+++ src/main/java/tr/com/huseyinaydin/expensemanager/dto/ExpenseFilterDTO.java
@@ -4,6 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+//بسم الله الرحمن الرحيم
+
+/**
+ *
+ * @author Huseyin_Aydin
+ * @since 1994
+ * @category Java, Spring Boot.
+ *
+ */
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
