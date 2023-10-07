951bd376869563a0448a70bffb417adec4f005db
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-10-07 15:39:15 +0300

    Harcama yöneticisi Web uygulamasıdır. Sisteme kayıt olan kullanıcı giriş yaptıktan sonra yaptığı harcalamaları ve hesaplamalarını yönetir. Spring Boot + Spring Security + Spring Data JPA + Validation + Model Mapper + MySQL + Postgresql + Thymeleaf + N-Tier Architechture + ORM + IoC + DI + Strategy Desing Pattern + Repository Desing Pattern teknolojileri ve teknikleri kullanılmıştır.

--- src/main/java/tr/com/huseyinaydin/expensemanager/repository/UserRepository.java
+++ src/main/java/tr/com/huseyinaydin/expensemanager/repository/UserRepository.java
@@ -5,6 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+//بسم الله الرحمن الرحيم
+
+/**
+ *
+ * @author Huseyin_Aydin
+ * @since 1994
+ * @category Java, Spring Boot.
+ *
+ */
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     //SELECT * FROM tbl_users WHERE email = ?
