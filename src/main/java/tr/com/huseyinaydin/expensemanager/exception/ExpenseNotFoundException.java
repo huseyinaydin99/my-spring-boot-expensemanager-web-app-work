f3222dd8eb14675b1dd44bed99cc7e4c57b92a65
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-10-07 15:36:26 +0300

    Harcama yöneticisi Web uygulamasıdır. Sisteme kayıt olan kullanıcı giriş yaptıktan sonra yaptığı harcalamaları ve hesaplamalarını yönetir. Spring Boot + Spring Security + Spring Data JPA + Validation + Model Mapper + MySQL + Postgresql + Thymeleaf + N-Tier Architechture + ORM + IoC + DI + Strategy Desing Pattern + Repository Desing Pattern teknolojileri ve teknikleri kullanılmıştır.

--- src/main/java/tr/com/huseyinaydin/expensemanager/exception/ExpenseNotFoundException.java
+++ src/main/java/tr/com/huseyinaydin/expensemanager/exception/ExpenseNotFoundException.java
@@ -2,6 +2,16 @@ package tr.com.huseyinaydin.expensemanager.exception;
 
 import lombok.Getter;
 
+//بسم الله الرحمن الرحيم
+
+/**
+ *
+ * @author Huseyin_Aydin
+ * @since 1994
+ * @category Java, Spring Boot.
+ *
+ */
+
 @Getter
 public class ExpenseNotFoundException extends RuntimeException{
     private String message;
