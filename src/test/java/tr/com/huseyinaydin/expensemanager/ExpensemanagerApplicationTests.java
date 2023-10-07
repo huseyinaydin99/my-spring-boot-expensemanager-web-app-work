b1d67d69bd7e3648ef29e68c52c0c3eebfdb0475
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-10-07 16:12:05 +0300

    Harcama yöneticisi Web uygulamasıdır. Sisteme kayıt olan kullanıcı giriş yaptıktan sonra yaptığı harcalamaları ve hesaplamalarını yönetir. Spring Boot + Spring Security + Spring Data JPA + Validation + Model Mapper + MySQL + Postgresql + Thymeleaf + N-Tier Architechture + ORM + IoC + DI + Strategy Desing Pattern + Repository Desing Pattern teknolojileri ve teknikleri kullanılmıştır.

--- src/test/java/tr/com/huseyinaydin/expensemanager/ExpensemanagerApplicationTests.java
+++ src/test/java/tr/com/huseyinaydin/expensemanager/ExpensemanagerApplicationTests.java
@@ -3,6 +3,16 @@ package tr.com.huseyinaydin.expensemanager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//بسم الله الرحمن الرحيم
+
+/**
+ *
+ * @author Huseyin_Aydin
+ * @since 1994
+ * @category Java, Spring Boot.
+ *
+ */
+
 @SpringBootTest
 class ExpensemanagerApplicationTests {
 
